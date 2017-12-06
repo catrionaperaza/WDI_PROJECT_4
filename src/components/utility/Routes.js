6209462401bb29c34f4d48602caf5ebf7cddafd2
JacieWDI <janischan@hotmail.com>
2017-12-06 16:16:42 +0000

    mac test

--- src/components/utility/Routes.js
+++ src/components/utility/Routes.js
@@ -4,11 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import Login    from '../auth/Login';
 import Register from '../auth/Register';
 
+import DinnersIndex from '../dinners/DinnersIndex';
+
 const Routes = () => {
   return (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/dinners" component={DinnersIndex} />
     </Switch>
   );
 };
