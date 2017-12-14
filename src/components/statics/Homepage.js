01f1822b0277f93f26c2d798559a828176ac1074
JacieWDI <janischan@hotmail.com>
2017-12-14 14:40:51 +0000

    style merging

--- src/components/statics/Homepage.js
+++ src/components/statics/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
 
-// import { Grid, Row, Col, Image } from 'react-bootstrap';
+
 
 import Auth from '../../lib/Auth';
 
@@ -29,24 +29,18 @@ class Homepage extends React.Component {
           <div className="page-banner col-md-12">
             <h1>Welcome to Ho Ho Hosts! </h1>
             <h2><em>Please click the icons to search for dinners or attendees for your dinner</em></h2>
-            { Auth.isAuthenticated() && <Link to={'/dinners'}><img src={'http://www.pvhc.net/img36/bmnfxduyaeokrqglccrw.png'} className="img-responsive icons" /></Link>}
-            { Auth.isAuthenticated() && <Link to={'/users'}><img src={'https://cdn3.iconfinder.com/data/icons/maps-and-navigation-7/65/68-512.png'} className="img-responsive icons" /></Link>}
+            { Auth.isAuthenticated() && <Link to={'/dinners'}><img src={'https://images.vexels.com/media/users/3/130189/isolated/preview/93b472a150245f86731d83b3f630ffb8-empty-dinner-plate-icon-by-vexels.png'} className="img-responsive icons" /></Link>}<br></br>
+            { Auth.isAuthenticated() && <Link to={'/users'}><img src={'http://www.roughleyinsurance.com/wp-content/uploads/2011/11/ServiceIcon-Group.png'} className="img-responsive icons" /></Link>}
           </div>
+          <br></br>
         </div>
 
-        {' '}
-        {' '}
-        {' '}
-        {' '}
 
         { this.state.user.dinnersCreated && this.state.user.dinnersCreated.map(dinner => {
           return(
             <div key={dinner.id} >
               <h2><Link to={`/dinners/${dinner.id}`}> Dinners I am hosting: {dinner.title}<img src={dinner.image} className="img-responsive image-tile" /></Link></h2>
 
-              {' '}
-              {' '}
-              {' '}
             </div>
           );
         })
@@ -54,7 +48,7 @@ class Homepage extends React.Component {
 
         { this.state.user.dinnersAttending && this.state.user.dinnersAttending.map(dinner => {
           return(
-            <div key={dinner.id} >
+            <div key={dinner.id} className="image-tile col-md-4 col-sm-6 col-xs-12">
               <h2><Link to={`/dinners/${dinner.id}`}><strong>Dinners I am Attending: {dinner.title}<img src={dinner.image}  className="img-responsive" /></strong></Link></h2>
             </div>
           );
