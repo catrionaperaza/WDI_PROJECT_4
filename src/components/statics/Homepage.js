cf6731d3ff4982ee1d6b5edb0b61dad5c9204c9e
odwyercatriona <odwyercatriona@gmail.com>
2017-12-15 13:26:51 +0000

    save intro

--- src/components/statics/Homepage.js
+++ src/components/statics/Homepage.js
@@ -27,7 +27,8 @@ class Homepage extends React.Component {
         <div className="row">
           <div className="page-banner col-md-12">
             <h1>Welcome to Ho Ho Hosts! </h1>
-            { !Auth.isAuthenticated() &&  <h2><em>Please login or register to search for dinners, or guests for your Christmas dinner</em></h2>}
+            { !Auth.isAuthenticated() && <h2>Please login or register to start your search for a dinner to attend, or for a guest(s) to join your party!</h2>}
+            { !Auth.isAuthenticated() &&  <h3><em>Ho Ho Hosts aims to match up Christmas dinners hosts with some extra space at their table this year with extra guests. Many of these guests are young professionals, living far from home and cannot make it back for Christmas, while we usually have families or groups of friends as hosts. It is a win/win situation as both gain from the interesting conversations and new friends made!</em></h3>}
             { !Auth.isAuthenticated() && <img src="https://www.englishandculture.com/hs-fs/hub/98462/file-984264747-jpg/images/how_to_talk_about_your_family_in_english.jpg?t=1433880746000" className="image-hp" />}
             { !Auth.isAuthenticated() && <img src="http://xmasblor.com/wp-content/uploads/2015/10/christmasweddingfood-14462908568gnk4.jpg" className="image-hp" />}
           </div>
